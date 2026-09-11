8efc3fcd7e6497eb685967408017816aa491f94f
Damian Wójcik <damian.wojcik@univio.com>
2026-09-11 08:53:11 +0200

    Releasing version 0.0.3

--- custom_components/settings_store/constants.py
+++ custom_components/settings_store/constants.py
@@ -2,7 +2,7 @@ NAME = 'Settings Store'
 DOMAIN = 'settings_store'
 PLATFORMS = ['sensor']
 AUTHOR = 'Damian Wójcik'
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 
 CONFIG_LABEL = 'label'
 CONFIG_NAME = 'name'
